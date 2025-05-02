bd4f880a623958a4e8d57461b34f61b83cf1d2a2
LaySatya <141922646+LaySatya@users.noreply.github.com>
2025-05-02 22:23:08 +0700

    time-and-rank-functions

---
+++ lib/ui/utils/time_utils.dart
@@ -0,0 +1,15 @@
+  
+   // Function to format the elapsed time to "HH:mm:ss"
+  String formatTime(int elapsedSeconds) {
+    final hours = (elapsedSeconds ~/ 3600).toString().padLeft(2, '0');
+    final minutes = ((elapsedSeconds % 3600) ~/ 60).toString().padLeft(2, '0');
+    final seconds = (elapsedSeconds % 60).toString().padLeft(2, '0');
+    return "$hours:$minutes:$seconds";
+  }
+
+  
+  String formatElapsedTime(int seconds) {
+    final minutes = (seconds ~/ 60).toString().padLeft(2, '0');
+    final remainingSeconds = (seconds % 60).toString().padLeft(2, '0');
+    return "$minutes:$remainingSeconds";
+  }
